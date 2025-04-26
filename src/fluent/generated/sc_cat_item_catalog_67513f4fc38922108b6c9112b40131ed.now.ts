5cf2f4de91da9f29538b60fe2e5c3388515a437d
System Administrator <admin@example.com>
2025-04-26 20:57:15 +0530

    Second Commit

---
+++ src/fluent/generated/sc_cat_item_catalog_67513f4fc38922108b6c9112b40131ed.now.ts
@@ -0,0 +1,11 @@
+import { Record } from '@servicenow/sdk/core'
+
+export default Record({
+    $id: '67513f4fc38922108b6c9112b40131ed',
+    table: 'sc_cat_item_catalog',
+    data: {
+        sc_cat_item: '3e217b4fc38922108b6c9112b4013125',
+        sc_catalog: 'e0d08b13c3330100c8b837659bba8fb4',
+        sys_name: 'Service Catalog.Test Item',
+    },
+})
---
+++ src/fluent/generated/sc_cat_item_category_23513f4fc38922108b6c9112b40131f2.now.ts
@@ -0,0 +1,11 @@
+import { Record } from '@servicenow/sdk/core'
+
+export default Record({
+    $id: '23513f4fc38922108b6c9112b40131f2',
+    table: 'sc_cat_item_category',
+    data: {
+        sc_cat_item: '3e217b4fc38922108b6c9112b4013125',
+        sc_category: '109f0438c6112276003ae8ac13e7009d',
+        sys_name: 'Services.Test Item',
+    },
+})
